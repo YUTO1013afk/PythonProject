b57d02e809e5e5a2c374829d335424a70d1751c8
githubアカウントのユーザー名 <github>
2022-04-19 21:41:18 +0900

    report_02_3.py

--- report_02_3.py
+++
@@ -1,9 +0,0 @@
-import math
-print(" x sin(x) cos(x) tan(x)")
-
-x = 0
-for n in range(7):
-    radian_angle = math.radians(x)
-    print(f'{x:02}' + " ", end="")
-    print(f'{math.sin(radian_angle):.4f} {math.cos(radian_angle):.4f} {math.tan(radian_angle):.4f}')
-    x = x + 15
