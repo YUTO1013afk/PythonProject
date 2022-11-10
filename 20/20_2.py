c5cb31acf1ff248fd68b6edc25efee3cd6ccd20f
優人 <yourentiandao304@gmail.com>
2022-11-10 12:34:10 +0900

    Commit

--- 20/20_2.py
+++ 20/20_2.py
@@ -3,9 +3,11 @@ import seaborn as sns
 list = []
 species = ['setosa', 'versicolor', 'virginica']
 df = sns.load_dataset('iris')
+
 # リストにspecies列の値を代入する
 for iris in df['species']:
     list.append(iris)
+
 # リストから欲しい値をカウントする
 for key in species:
     val = list.count(key)
---
+++ 20/20_3.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_spli
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn .datasets import load_iris
+
