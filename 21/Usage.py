d88bbb6132c41376f58f6c5155893e3297147727
優人 <yourentiandao304@gmail.com>
2022-11-17 10:04:38 +0900

    Commit

---
+++ 21/Usage.py
@@ -0,0 +1,15 @@
+from dtreeviz.trees import *
+from sklearn import tree
+from sklearn.datasets import *
+
+regr = tree.DecisionTreeRegressor(max_depth=2)
+boston = load_boston()
+regr.fit(boston.data, boston.target)
+
+viz = dtreeviz(regr,
+               boston.data,
+               boston.target,
+               target_name='price',
+               feature_names=boston.feature_names)
+              
+viz.view()              
---
+++ 21/dtreeviz test.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import *
+from sklearn import tree
+from dtreeviz.trees import *
+
+classifier = tree.DecisionTreeClassifier(max_depth=2)  # limit depth of tree
+iris = load_iris()
+classifier.fit(iris.data, iris.target)
+
+viz = dtreeviz(classifier, 
+               iris.data, 
+               iris.target,
+               target_name='variety',
+               feature_names=iris.feature_names, 
+               class_names=["setosa", "versicolor", "virginica"]  # need class_names for classifier
+              )  
+              
+viz.view() 
