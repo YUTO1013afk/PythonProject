5704031e52231395214bf1c9107ed05bdd3a399c
優人 <yourentiandao304@gmail.com>
2022-12-15 13:07:33 +0900

    Commit

--- 25/19_10.py
+++ 25/19_10.py
@@ -1,15 +1,15 @@
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 from keras.datasets import mnist
-from keras.layers import Activation, Dense, Dropout
-from keras.models import Sequential, load_model
+import os
+from keras.layers import Activation, Dense
+from keras.models import Sequential
 from keras import optimizers
 from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
 # ライブラリーの重複を検出しても処理を進めるというもの
 os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
 X_train = X_train.reshape(X_train.shape[0], 784)[:6000]
 X_test = X_test.reshape(X_test.shape[0], 784)[:1000]
 y_train = to_categorical(y_train)[:6000]
@@ -20,20 +20,20 @@ model.add(Dense(256, input_dim=784))
 model.add(Activation("sigmoid"))
 model.add(Dense(128))
 model.add(Activation("sigmoid"))
-model.add(Dropout(rate=0.5))
 model.add(Dense(10))
 model.add(Activation("softmax"))
 
-sgd = optimizers.SGD(lr=0.1)
-model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=["accuracy"])
-
-# epochs数は5を指定します。
-history = model.fit(X_train, y_train, batch_size=500, epochs=5, verbose=1, validation_data=(X_test, y_test))
-
-# accuracy、val_accuracyのプロットです
+model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"])
+# ---------------------------
+# ここにコードを記述してください
+history = model.fit(X_train, y_train, epochs=11)
+print("evaluate loss: {0[0]}\nevaluate acc: {0[1]}".format(score))
+# ---------------------------
+# acc、val_accのプロット
 plt.plot(history.history["accuracy"], label="acc", ls="-", marker="o")
-plt.plot(history.history["val_accuracy"], label="val_acc", ls="-", marker="x")
-plt.ylabel("accuracy")
+plt.plot(history.history["loss"], label="loss", ls="-", marker="o")
+plt.ylabel("accuracy loss")
 plt.xlabel("epoch")
 plt.legend(loc="best")
+plt.grid(True)
 plt.show()
--- 25/19_8.py
+++ 25/19_8.py
@@ -1,38 +1,43 @@
-import numpy as np
-import os
-import matplotlib.pyplot as plt
 from keras.datasets import mnist
-from keras.layers import Activation, Dense, Dropout
-from keras.models import Sequential, load_model
-from keras import optimizers
+import os
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.utils.vis_utils import plot_model
 from keras.utils.np_utils import to_categorical
-# ライブラリーの重複を検出しても処理を進めるというもの
+import matplotlib.pyplot as plt
+# ライブラリの重複を検出しても処理を進めるというもの
 os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
 X_train = X_train.reshape(X_train.shape[0], 784)[:6000]
 X_test = X_test.reshape(X_test.shape[0], 784)[:1000]
 y_train = to_categorical(y_train)[:6000]
 y_test = to_categorical(y_test)[:1000]
 
 model = Sequential()
+# 入力ユニット数は 784、1つ目の全結合層の出力ユニット数は 256 です
 model.add(Dense(256, input_dim=784))
 model.add(Activation("sigmoid"))
+
+# 2つ目の全結合層の出力ユニット数は 128 です
+# ---------------------------
+# ここにコードを記述してください
 model.add(Dense(128))
 model.add(Activation("sigmoid"))
-model.add(Dropout(rate=0.5))
+# ---------------------------
+
+# 3つ目の全結合層（出力層）の出力ユニット数は 10 です
 model.add(Dense(10))
 model.add(Activation("softmax"))
 
-sgd = optimizers.SGD(lr=0.1)
-model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"])
 
 # epochs数は5を指定します。
-history = model.fit(X_train, y_train, batch_size=500, epochs=5, verbose=1, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, verbose=1, epochs=5)
 
 # accuracy、val_accuracyのプロットです
 plt.plot(history.history["accuracy"], label="acc", ls="-", marker="o")
-plt.plot(history.history["val_accuracy"], label="val_acc", ls="-", marker="x")
 plt.ylabel("accuracy")
 plt.xlabel("epoch")
 plt.legend(loc="best")
