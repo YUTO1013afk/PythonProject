34305763556deba043935a2fd7073d8ef47d145e
YUTO1013afk <yourentiandao@gmail.com>
2023-01-26 12:18:16 +0900

    Commit

--- 28/vgg16.py
+++ 28/vgg16.py
@@ -5,7 +5,7 @@ from keras.applications.vgg16 import decode_predictions
 import numpy as np
 import keras
 
-img_path = 'pexels-tobias-bjørkli-1693095.jpg'
+img_path = 'apple.jpg'
 
 img = keras.utils.load_img(img_path, target_size=(224, 224))
 img = keras.utils.img_to_array(img)
